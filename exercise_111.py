639c86fe909c90e328f81a88346ca7b918df79b0
Avraj Singh <ASingh@spartaglobal.com>
2020-02-06 15:53:55 +0000

    exercise_111

---
+++ exercise_111.py
@@ -0,0 +1,38 @@
+import math
+
+
+# define a multiply function
+
+def mult(num1, num2):
+    return num1 * num2
+
+
+# define a dived function
+
+def div(num1, num2):
+    return num1 / num2
+
+
+# define a multiply function
+
+def sub(num1, num2):
+    return num1 - num2
+
+
+# define a multiply function
+
+def add(num1, num2):
+    return num1 + num2
+
+
+# define area of a circle function
+
+def area_of_circle(num1, num2):
+    return (num1 * num2) * math.pi
+
+
+def area_of_triangle(num1, num2):
+    return 0.5 * (num1 * num2)
+
+
+print(add(1, 2))
